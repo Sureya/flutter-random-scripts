d05e98000f52c5aebb2d668c3d59d4eae7f60940
Sureya Sathiamoorthi <catchsureya@gmail.com>
2021-11-26 16:10:15 +0000

    Create context_extension.dart

---
+++ context_extension.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+extension ContextExtension on BuildContext {
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+
+  TextTheme get textTheme => Theme.of(this).textTheme;
+  TextTheme get primaryTextTheme => Theme.of(this).primaryTextTheme;
+
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
+
+  ThemeData get appTheme => Theme.of(this);
+  MaterialColor get randomColor => Colors.primaries[Random().nextInt(17)];
+
+  bool get isKeyBoardOpen => MediaQuery.of(this).viewInsets.bottom > 0;
+  double get elevation => 10;
+  Brightness get appBrightness => MediaQuery.of(this).platformBrightness;
+}
+
+extension MediaQueryExtension on BuildContext {
+  double get height => mediaQuery.size.height;
+  double get width => mediaQuery.size.width;
+
+  double get lowValue => height * 0.01;
+  double get normalValue => height * 0.02;
+  double get mediumValue => height * 0.04;
+  double get highValue => height * 0.1;
+
+  double dynamicWidth(double val) => width * val;
+  double dynamicHeight(double val) => height * val;
+}
+
+
+
+void main() {
+  runApp(TestApp());
+}
+
+class TestApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "SwimBooker",
+      home: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: context.dynamicHeight(0.3),
+              width: context.dynamicHeight(0.5),
+              color: Colors.blue,
+            )
+          ],
+        ),
+      ),
+    );
+
+
+  }
+}
+
